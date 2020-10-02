962449f1b074a2371c083b68161df64dbc7a94b0
tiny-trevor <naomi.e.trevor@gmail.com>
2020-10-02 20:08:08 +0100

    Initial Setup

---
+++ console.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+
+<?php
+    
+    require_once __DIR__ . '/vendor/autoload.php';
+    
+    use Symfony\Component\Console\Application;
+    
+    $console = new Application();
+    $console->run();
+    
+
+    
+    
+    
