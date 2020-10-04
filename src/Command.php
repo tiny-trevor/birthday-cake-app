f35d5f184a42d8b7186e5e61627a0f856ce54f68
tiny-trevor <naomi.e.trevor@gmail.com>
2020-10-04 21:00:44 +0100

    feat: no two cake days can occur in a row, the day after each cake day must be a cake free day.

--- src/Command.php
+++ src/Command.php
@@ -14,10 +14,16 @@
          */
         public const types = ['txt', 'csv'];
         
-        
+        public array $company_holidays;
+    
+    
         public function __construct()
         {
             parent::__construct();
+            
+            //Get an array of company holidays
+            $this->company_holidays = $this->getCompanyHolidays();
+    
         }
     
         /**
@@ -43,7 +49,8 @@
             $output->writeln($fileContents);
             
         }
-        
+    
+        //TODO: Rename function after proper purpose
         private function getData(String $filepath)
         {
             
@@ -51,9 +58,9 @@
                 $data = $this->extractData($filepath);
                 $company_holidays_data = $this->mapCompanyHolidays($data);
                 $skip_birthdays = $this->skipBirthday($company_holidays_data);
-                //TODO: No two cake days in a row.
-                $get_cakedays = $this->getCakeDays($skip_birthdays);
-                
+                $next_working_days = $this->getNextWorkingDays($skip_birthdays);
+                $get_cakedays = $this->getCakeDays($next_working_days);
+
                 $csv = $this->createCSV($get_cakedays);
                 
                 return $csv;
@@ -149,7 +156,8 @@
             $company_holidays = $this->getCompanyHolidays();
             
             $holidays_mapped = [];
-              
+            
+            //TODO: Rename to birthday
             foreach($data as $name => $full_date) {
                 
                 while(in_array($full_date->format('m-d'), $company_holidays) || $full_date->isWeekend()) {
@@ -196,48 +204,39 @@
             return $birthday_skipped;
         
         }
-    
+        
         /**
+         * TODO: Update docs
          * From the data provided, iterate through each given date to find cake recipients, and create list of cake days
          * small cakes or large cakes, as well as the names of those getting cake that day
          *
          * @param $data
          * @return mixed
          */
-        public function getCakeDays($data)
+        public function getNextWorkingDays($data)
         {
             $dates = array_values($data);
         
+            //TODO: Remove Duplicates
             foreach($dates as $carbonDate) {
                 $full_date = $carbonDate->format('Y-m-d');
             
-                $cake_days[$full_date]['date'] = $full_date;
-                $cake_days[$full_date]['sm'] = 0;
-                $cake_days[$full_date]['lg'] = 0;
-                $cake_days[$full_date]['names'] = '';
-                $cakes = 0;
+                $working_days[$full_date]['date'] = $full_date;
+                $working_days[$full_date]['cakes'] = 0;
+                $working_days[$full_date]['names'] = '';
             
                 foreach($data as $name => $date)
                 {
                     if($carbonDate == $date) {
-                        $cake_days[$full_date]['names'] = $cake_days[$full_date]['names'] == '' ? $name : $cake_days[$full_date]['names'] . ", " . $name;
-                        $cakes++;
+                        $working_days[$full_date]['names'] = $working_days[$full_date]['names'] == '' ? $name : $working_days[$full_date]['names'] . ", " . $name;
+                        $working_days[$full_date]['cakes']++;
                     }
-                }
-            
-                if($cakes > 1) {
-                    $cake_days[$full_date]['sm'] = 0;
-                    $cake_days[$full_date]['lg'] = 1;
-                }
-                else if($cakes == 1) {
-                    $cake_days[$full_date]['sm'] = 1;
-                    $cake_days[$full_date]['lg'] = 0;
                 }
             }
             
-            usort($cake_days, array($this,'sort_dates'));
+            usort($working_days, array($this,'sort_dates'));
         
-            return $cake_days;
+            return $working_days;
         }
     
         /**
@@ -254,6 +253,119 @@
                 return $t1 - $t2;
         }
     
+        /**
+         * Given an array of all cakedays, apply the health conditions:
+         * No two cake days can occur after one another, instead a large cake is provided on the second day
+         * The day after a cake day is a cake-free day. Any cake due on a cake-free day should be moved to the next day
+         * Once satisfied, return the finalised list of cake days.
+         *
+         * @param $data
+         * @return array
+         * @throws \Exception
+         */
+        public function getCakeDays($data)
+        {
+            
+            // Get all cake days into an array
+            $dates = array_column($data, 'date');
+            
+            // For health reasons, the day after each cake must be cake-free.
+            $cake_free_days = [];
+            
+            // final multidimensional cake days array, initialised with key 0
+            $cake_days = [];
+            $array_num = 0;
+            
+            //Iterate over each cake day entry
+            foreach($data as $cakeday) {
+                
+                //Get current day
+                $current_day = $cakeday['date'];
+    
+                //Get next day
+                $next_day = (new Carbon($cakeday['date']))->addDay()->format('Y-m-d');
+    
+                //If two or more cakes days coincide, we instead provide one large cake to share.
+                if($cakeday['cakes'] > 1) {
+                    $sm = 0;
+                    $lg = 1;
+                }
+                else {
+                    $sm = 1;
+                    $lg = 0;
+                }
+                
+                // For health reasons, the day after each cake must be cake-free.
+                $cake_free_day = (new Carbon($next_day))->addDay()->format('Y-m-d');
+    
+                // If there is to be cake two days in a row, we instead provide one large cake on the second day.
+                // Any cakes due on a cakefree day are postponed to the next working day.
+                if(in_array($next_day, $dates) && !in_array($next_day, $cake_free_days)) {
+    
+                    // Set next day date as date variable
+                    $date = $next_day;
+                    
+                    // Provide one large cake on the second day, and set the small cakes to 0
+                    $lg = 1;
+                    $sm = 0;
+                    
+                    // Find data object for the next day
+                    $next_cakeday = array_search($next_day, array_column($data, 'date'));
+    
+                    // Set current cakeday names as well as names for next cakeday as names variable
+                    $names = $cakeday['names'] . ', '.$data[$next_cakeday]['names'];
+    
+                    // For health reasons, the day after each cake must be cake-free.
+                    $cake_free_days[] = $cake_free_day;
+                    
+                }
+                // Any cakes due on a cakefree day are postponed to the next working day.
+                else if(in_array($current_day, $cake_free_days)) {
+    
+                    // Postpone to next working day
+                    $skip_day = new Carbon($current_day);
+                    do {
+                        $skip_day->addDay();
+                    } while(in_array($skip_day->format('m-d'), $this->company_holidays) || $skip_day->isWeekend());
+                    
+                    // Set next day date as date variable
+                    $date = $skip_day->format('Y-m-d');
+    
+                    // Set names for cakeday as names variable
+                    $names = $cakeday['names'];
+    
+                    // For health reasons, the day after each cake must be cake-free.
+                    $cake_free_days[] = $cake_free_day;
+                    
+                }
+                else {
+                    // If the current cakeday date has already been processed by the previous two-day clause, skip
+                    if(array_search($cakeday['date'], array_column($cake_days, 'date')))
+                    {
+                        continue;
+                    }
+    
+                    // Set cakeday date as date variable
+                    $date = $cakeday['date'];
+                    
+                    // Set names for cakeday as names variable
+                    $names = $cakeday['names'];
+                }
+    
+                // Assign values to new array item
+                $cake_days[$array_num]['date'] = $date;
+                $cake_days[$array_num]['sm'] = $sm;
+                $cake_days[$array_num]['lg'] = $lg;
+                $cake_days[$array_num]['names'] = $names;
+                
+                // Increment array_num
+                $array_num++;
+                
+            }
+            
+            return $cake_days;
+        }
+        
         /**
          * Create CSV file in current main directory (/birthday-cake) out of cake day data
          * Filename: 'cakedays-d-m-Y.csv'
@@ -334,12 +446,13 @@
          * A Date in the format of dddd-dd-dd
          * And that the line ends after that
          *
+         * TODO: Rename function after proper purpose
+         *
          * @param $file_line
          * @return bool
          */
         private function validateData($file_line)
         {
-            //TODO: Date must be a real date.
             $line_pattern = '/((,)(\s*)(\d{4})(-\d{2}){2})$/';
     
             if(preg_match($line_pattern, $file_line)){
